81b48a08ef4ac722b8491e9f59bdacf7a58937ea
huytakazy55 <khongthaydoi124@gmail.com>
2023-07-31 13:58:47 +0700

    update home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
                     </li>
                 </ul>
 
-                <i className="uil uil-times nav__close"></i>
+                <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
             </div>
 
             <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
